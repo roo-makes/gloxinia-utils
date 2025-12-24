4a98601c547eeb304c944a73c8928bc158e8f888
Andrew Metcalf <met5678@gmail.com>
2025-12-24 14:15:31 -0500

    Chore: Allow extending `commander` program setup

--- src/cli-scripts/utils/setup-program.ts
+++ src/cli-scripts/utils/setup-program.ts
@@ -1,11 +1,17 @@
 import { Command } from "commander";
 
-export const setupProgram = (): Command => {
+export const setupProgram = (
+  onBeforeParse?: (program: Command) => void
+): Command => {
   const program = new Command();
   program
     .requiredOption("-i, --input <inputs...>", "input file or directory(s)")
     .requiredOption("-o, --output <output>", "output base directory")
     .option("--inputBasePath <inputBasePath>", "input base directory");
+
+  if (onBeforeParse) {
+    onBeforeParse(program);
+  }
 
   program.parse(process.argv);
 
